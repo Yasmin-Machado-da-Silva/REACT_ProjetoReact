ba6582dfae41e9d5a1d6d61c46fe1551713a86d7
Yasmin Machado <yasmin.m.silva14@aluno.senai.br>
2025-04-22 16:46:08 -0300

    FEAT: Adicionando components, pages e assets

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme';
+// import Login from './pages/login/Login';
+
+function App() {
+  return (
+  <>
+    {/* <Login/> */}
+    <CadastroFilme/>
+  </>
+);
+}
+
+export default App;
